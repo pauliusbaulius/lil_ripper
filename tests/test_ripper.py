5da2c44e52d6d4d5937cfccc606ade18b0f85206
pauliusbaulius@thinkpad <paulius.gerve@protonmail.com>
2020-05-05 01:00:55 +0200

    adding tests, reverse tdd

--- tests/test_ripper.py
+++ tests/test_ripper.py
@@ -1,6 +1,11 @@
 from src import ripper
 
 
+def test_create_dir():
+    # TODO how to create temp dir for testing purposes and delete after?
+    pass
+
+
 def test_is_downloadable():
     # Should work with "." too.
     formats = [".jpg", "webm", "png"]
@@ -12,3 +17,22 @@ def test_is_downloadable():
     assert ripper.is_downloadable(link_two, formats) is True
     assert ripper.is_downloadable(link_three, formats) is False
     assert ripper.is_downloadable(link_four, formats) is True
+    assert ripper.is_downloadable(".jpg", ["jpeg"]) is False
+    assert ripper.is_downloadable(".jpg", ["jpg"]) is True
+    assert ripper.is_downloadable(".jpg.webm", ["jpg"]) is False
+
+
+def test_check_if_downloaded():
+    is_downloaded = ripper.check_if_downloaded("test_dir/", "test.jpeg")
+    assert is_downloaded
+
+    is_downloaded = ripper.check_if_downloaded("test", "test.jpeg")
+    assert is_downloaded is None
+
+    is_downloaded = ripper.check_if_downloaded("test_dir", "test.gif")
+    assert is_downloaded is None
+
+
+def test_load_csv():
+    subreddits = ripper.load_csv("test_dir/subreddits.csv")
+    assert ['news', 'linux', 'programmingcirclejerk'] == subreddits
